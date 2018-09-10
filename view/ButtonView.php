30fcb20d33fbb62104369146242024641b0b3830
antonstrand <jag@antonstrand.se>
2018-09-10 13:55:24 +0200

    Init

---
+++ view/ButtonView.php
@@ -0,0 +1,27 @@
+<?php
+
+class ButtonView
+{
+
+    /*
+    One render function to render everything to
+    ther client in the beginning.
+
+    Function returns 25 buttons.
+     */
+    public function render()
+    {
+        $alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        $characterButton = '';
+        $buttonList = '';
+
+        // Generate button list
+        foreach ($alphabet as $letter) {
+            $characterButton = '<input name="character" type="submit" value="' . $letter . '">  </input>';
+            $buttonList .= $characterButton;
+        }
+
+        return $buttonList;
+    }
+
+}
---
+++ view/HangmanView.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+One render function to render everything to
+ther client in the beginning
+ */
+class HangmanView
+{
+}
+//   public function render($buttons) {
+
+//   }
+
+// var_dump($_GET['character']);
